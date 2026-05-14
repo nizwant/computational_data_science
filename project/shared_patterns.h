fbc3c3bc3e8a90ac312386f996ab82fb3db146ea
nizwant <mateusznizwantowski@gmail.com>
2026-05-14 12:17:48 +0200

    feat: include hash map implementation and create user struct

--- project/shared_patterns.h
+++ project/shared_patterns.h
@@ -7,6 +7,8 @@
 #include <sys/select.h>
 #include <time.h>
 
+#include "uthash.h"
+
 #define MAX_MESS_SIZE 1024
 
 
@@ -22,10 +24,20 @@ typedef enum _packet_type {
 
 typedef struct _packet_header{
     PacketType type;
-    char my_username[32];
+    char sender_username[32];
 } PacketHeader;
 
 typedef struct _packet{
     PacketHeader header;
     char message[MAX_MESS_SIZE];
 } Packet;
+
+typedef struct _client{
+    char username[32];
+    struct in_addr ip_addr;
+    uint16_t port;
+    time_t last_time_seen;
+    char password[32]; //SHA-256 of password
+    char public_key[32]; // ED25519 key used as a authentication method 
+    UT_hash_handle hh;
+} Client;
